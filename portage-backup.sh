3d31258e48cced3aa1e900bdbd589906b2569f67
Michał Łyszczek <michal.lyszczek@bofc.pl>
2020-01-30 09:35:13 +0100

    portage-backup.sh: add 'world' file to backup
    Signed-off-by: Michał Łyszczek <michal.lyszczek@bofc.pl>

--- portage-backup.sh
+++ portage-backup.sh
@@ -10,10 +10,14 @@ tmp_file=$(mktemp)
 
 # create backup
 tar -cJpf "${DESTDIR}/${now}.tar.xz" -C /etc portage
+cat /var/lib/portage/world | gzip > "${DESTDIR}/world.${now}.gz"
+cat /var/lib/portage/world_sets | gzip > "${DESTDIR}/world_sets.${now}.gz"
 
 # fix permissions
-chown ${USER} "${DESTDIR}/${now}.tar.xz"
-chmod ${MOD} "${DESTDIR}/${now}.tar.xz"
+chown ${USER} "${DESTDIR}/${now}.tar.xz" "${DESTDIR}/world.${now}.gz" \
+		"${DESTDIR}/world_sets.${now}.gz"
+chmod ${MOD} "${DESTDIR}/${now}.tar.xz" "${DESTDIR}/world.${now}.gz" \
+		"${DESTDIR}/world_sets.${now}.gz"
 
 # and cleanup
 rm -f ${tmp_file}
